9e2302f7322ba4a488be5b0cdfe000d7e2723ad4
Kaung Zin Hein <kaungzinhein113@gmail.com>
2024-08-10 20:31:20 -0400

    initial setup
    Signed-off-by: Kaung Zin Hein <kaungzinhein113@gmail.com>

---
+++ go.mod
@@ -0,0 +1,3 @@
+module parse-for-range-occurence
+
+go 1.22.5
---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"regexp"
+)
+
+func checkError(err error){
+
+	if err != nil{
+		panic(err)
+	}
+}
+func main()  {
+	
+	file, err := os.Open("rg-for-range.txt")
+	checkError(err)
+
+	defer file.Close()
+
+	re, err := regexp.Compile(`(.*)\.go:\d+:`)
+	checkError(err)
+
+	scanner := bufio.NewScanner(file)
+
+	lineCount := 0
+
+    filesMap := make(map[string]int)
+
+	for scanner.Scan() {
+		line :=scanner.Text()
+		lineCount++
+
+		matches := re.FindStringSubmatch(line)
+		if matches == nil {
+			panic(errors.New("no match"))
+		}
+
+		// store in file map
+		fileName := matches[1] + ".go"
+
+		_, ok := filesMap[fileName]
+
+		if(ok){
+			filesMap[fileName]++
+		}else{
+			filesMap[fileName] = 1
+		}
+
+	}
+
+	for fileName, count := range filesMap {
+        fmt.Printf("%s: %d\n", fileName, count)
+    }
+
+	fmt.Printf("Unique files: %d\n", len(filesMap))
+
+	scannerErr := scanner.Err()
+
+	checkError(scannerErr)
+}
